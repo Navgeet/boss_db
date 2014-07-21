f57c6622d010aaf005f0834f346eca8b2b2ac289
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2014-07-21 15:59:00 +0530

    try restarting memcached process once before throwing up

--- src/cache_adapters/boss_cache_adapter_memcached_bin.erl
+++ src/cache_adapters/boss_cache_adapter_memcached_bin.erl
@@ -15,19 +15,44 @@ start(Options) ->
 stop(_Conn) ->
     erlmc:quit().
 
-init(_Options) ->
-    {ok, undefined}.
+init(Options) ->
+    {ok, Options}.
 
 terminate(Conn) ->
     stop(Conn).
 
-get(_Conn, Prefix, Key) ->
-    case erlmc:get(term_to_key(Prefix, Key)) of
-        <<>> -> 
-            undefined;
-        Bin -> 
-            binary_to_term(Bin)
-    end.
+get_safe(Conn, Prefix, Key) ->
+  case erlmc:get(term_to_key(Prefix, Key)) of
+    <<>> -> undefined;
+    Bin -> binary_to_term(Bin)
+  end.
+
+refresh_memcached_process(Host, Port, PoolSize) ->
+  lager:info("Refreshing memcached. host ~p, port ~p, poolsize ~p",
+             [Host, Port, PoolSize]),
+  ok = erlmc:refresh_server(Host, Port, PoolSize),
+  timer:sleep(1000),
+  LiveConnections = length(ets:match(erlmc_connections,
+                                     {{Host, Port}, '$1'})),
+  case LiveConnections =:= PoolSize of
+    true -> lager:info("Refreshing memcached succeeded"),
+            ok;
+    false -> lager:info("Refreshing memcached failed"),
+             error
+  end.
+
+get(Conn, Prefix, Key) ->
+  try erlmc:get(term_to_key(Prefix, Key)) of
+      <<>> -> undefined;
+      Bin -> binary_to_term(Bin)
+  catch exit:erlmc_continuum_empty ->
+      [{Host, Port, PoolSize}] =
+        proplists:get_value(cache_servers, Conn, [{"localhost", 11211, 1}]),
+      case refresh_memcached_process(Host, Port, PoolSize) of
+        ok -> get_safe(Conn, Prefix, Key);
+        error -> exit(erlmc_continuum_empty)
+      end
+  end.
 
 set(_Conn, Prefix, Key, Val, TTL) ->
     erlmc:set(term_to_key(Prefix, Key), term_to_binary(Val), TTL).
